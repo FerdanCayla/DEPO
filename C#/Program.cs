8c73c8b07c96915a8c3ad9c1d6d03a6e6de664a0
Ferdan Cayla <ferdancayla@gmail.com>
2025-03-25 19:07:13 +0300

    1

---
+++ C#/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int dogumYili;
+
+Console.WriteLine("Doğum Yılı: ");
+dogumYili=Convert.ToInt32( Console.ReadLine() );
+
+Console.WriteLine("Yaşınız: " + (2025 - dogumYili));
+
+
+
+Console.ReadKey();
