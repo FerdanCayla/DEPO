680256c6b473b55d0a43a9d3e6816b91e1c7c8d6
Ferdan Cayla <ferdancayla@gmail.com>
2025-04-02 21:10:22 +0300

    333

--- C#/obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ C#/obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1031045f17445e22c05dd8449ccf3b4a9aa90fa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a981423bce9dc1bd78513874bf13c706d632aee7")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
