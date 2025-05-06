7976224889e1e06ec1938ae5582f33aeb3e13a5c
FerdanCayla <ferdancayla@gmail.com>
2025-05-06 22:39:44 +0300

    132

--- Arrays/obj/Debug/net9.0/Arrays.AssemblyInfo.cs
+++ Arrays/obj/Debug/net9.0/Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e38cee56f397d31d1cb4e632d98a1afcc6bb75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c170ff910d1ba7e9808a41b4963ebdaef6c32983")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
