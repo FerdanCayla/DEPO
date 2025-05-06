c170ff910d1ba7e9808a41b4963ebdaef6c32983
FerdanCayla <ferdancayla@gmail.com>
2025-05-06 22:26:03 +0300

    111

--- DiceRoller/DiceRoller/Program.cs
+++ DiceRoller/DiceRoller/Program.cs
@@ -1,4 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random numberGen = new Random();
+int roll01=0;
+int roll02=1;
+int attempts=0;
+Console.WriteLine("Enter the roll");
+
+while (roll01!=roll02)
+{
+    Console.ReadKey();
+    roll01 = numberGen.Next(1,7);
+    roll02 = numberGen.Next(1,7);
+    attempts++;
+    Console.WriteLine("You Rolled: "+ roll01 + "-" + roll02);
+}
+
+Console.WriteLine("It took " + attempts + " tries to roll two of a kind.");
 
 Console.ReadKey();
